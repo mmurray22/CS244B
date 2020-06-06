87d28091e7ac2d9bd87f0d6c745165da9c6db0ce
guy <gwuollet@stanford.edu>
2020-06-06 15:58:48 -0400

    aws scripts

---
+++ aws/kademlia_scripts/first_node.py
@@ -0,0 +1,30 @@
+import logging
+import asyncio
+
+from kademlia.network import Server
+
+if len(sys.argv) != 3:
+    print("Usage: python run_server.py <ksize> <alpha>")
+    sys.exit(1)
+
+handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+log = logging.getLogger('kademlia')
+log.addHandler(handler)
+log.setLevel(logging.DEBUG)
+
+
+loop = asyncio.get_event_loop()
+loop.set_debug(True)
+
+server = Server( ksize = int(sys.argv[1]), alpha = int(sys.argv[2]) )
+loop.run_until_complete(server.listen(8468))
+
+try:
+    loop.run_forever()
+except KeyboardInterrupt:
+    pass
+finally:
+    server.stop()
+    loop.close()
