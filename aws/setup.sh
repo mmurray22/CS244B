6bcacc4df8f88fe9ffa6e781b8592812f0adfeaa
guy <gwuollet@stanford.edu>
2020-06-11 02:30:23 -0400

    cleaning up repo

--- aws/setup.sh
+++ aws/setup.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
 # I need some way to say yes to all prompts
-
 sudo yum install python37
 curl -O https://bootstrap.pypa.io/get-pip.py
-python3 get-pip.py --user
+python3 get-pip.py
 
 sudo yum install git -y
 sudo yum update -y
 
-# clone the kademlia repo and change it to make it work
-git clone https://github.com/bmuller/kademlia
-cd kademlia
-pip install -r dev-requirements.txt
-pip install rpcudp
-pytest
+git clone https://github.com/mmurray22/CS244B
